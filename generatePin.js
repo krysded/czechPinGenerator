b64e7b4c3125adff9b2d308d50aa771f8eba4efc
Vim noob <krysded@gmail.com>
2024-02-03 21:48:05 +0100

    It works but with severe restrictions.

---
+++ generatePin.js
@@ -0,0 +1,22 @@
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
+    function randomFifty(){
+        return Math.round(Math.random()) * 50
+    }
+    function randomMonth(){
+        return randomNumber(1, 13) + randomFifty()
+    }
+    function getRandomThreeDigits(){
+        return "" + randomNumber(0, 10) + randomNumber(0, 10) + randomNumber(0, 10)
+    }
+    function getRandomDate(){
+        return "" + randomNumber(50, 100) + randomMonth() + randomNumber(1, 28) 
+    }
+  function generatePIN(){ 
+        let pin = "" + getRandomDate() + getRandomThreeDigits()
+        let lastDigit = pin % 11
+        return "" + pin + lastDigit
+  }
+
+console.log(generatePIN());
