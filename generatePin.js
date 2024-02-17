cdbc96636b0469d27cf2185b6807371c13d4f0f3
Vim noob <krysded@gmail.com>
2024-02-17 19:20:10 +0100

    Better version of getRandomDate()

--- generatePin.js
+++ generatePin.js
@@ -1,31 +1,24 @@
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
-    function randomFifty(){
-        return Math.round(Math.random()) * 50
-    }
-    function randomMonth(){
-        return randomNumber(1, 13) + randomFifty()
-    }
-    function getRandomThreeDigits(){
-        return "" + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9)
-    }
-    function getRandomDate(minAge, maxAge){
-	    let birthDate = new Date()
-	    let rndYear = randomNumber(minAge, maxAge)
-	    if (rndYear == minAge){
-		    birthDate.setFullYear(birthDate.getFullYear() - rndYear, randomNumber(0, birthDate.getMonth()), randomNumber(0, birthDate.getDay()))
-	    } else if (rndYear == maxAge){
-		    birthDate.setFullYear(birthDate.getFullYear() - rndYear, randomNumber(birthDate.getMonth(), 11), randomNumber(birthDate.getDay(), 31))
-	    } else {
-		    birthDate.setFullYear(birthDate.getFullYear() - rndYear, randomNumber(0, 11), randomNumber(0, 31))
-	    }
-        return "" + randomNumber(50, 100) + randomMonth() + randomNumber(1, 28) 
-    }
-  function generatePIN(){ 
-        let pin = "" + getRandomDate() + getRandomThreeDigits()
-        let lastDigit = pin % 11
-        return "" + pin + lastDigit
-  }
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function randomFifty(){
+	return Math.round(Math.random()) * 50
+}
+function getRandomThreeDigits(){
+	return "" + randomNumber(0, 9) + randomNumber(0, 9) + randomNumber(0, 9)
+}
+function getRandomDate(minAge, maxAge){
+	let birthDate = new Date()
+	let rndYear = randomNumber((minAge *  31556952000), (maxAge * 31556952000 ))
+	return rndYear = birthDate.getTime() - rndYear
+}
+function getPinDate(minAge, maxAge){
+	let rndDate = getRandomDate(minAge, maxAge). 
+}
+function generatePIN(){ 
+	let pin = "" + getRandomDate() + getRandomThreeDigits()
+	let lastDigit = pin % 11
+	return "" + pin + lastDigit
+}
 
-console.log(generatePIN());
+console.log(getRandomDate(18, 25));
